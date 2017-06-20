fe06f225ecebcd5ef8ff7bd34064c5e6f112429f
周林华 <zhoulinhua@gwi.com.cn>
2017-06-20 11:41:32 +0800

    添加log4j

--- src/gwi/zlh/study/Login.java
+++ src/gwi/zlh/study/Login.java
@@ -1,5 +1,6 @@
 package gwi.zlh.study;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +10,17 @@ import gwi.zlh.model.User;
 
 @Controller
 public class Login {
+	private static Logger logger=Logger.getLogger(Login.class);
+	
     @RequestMapping("/loginIndex")
     public String loginIndex() {
-	System.out.println("loginIndex");
+	System.out.println("System.out.println-loginIndex");		
+	logger.fatal("logger.fatal-loginIndex");
+	logger.error("logger.error-loginIndex");
+	logger.warn("logger.warn-loginIndex");
+	logger.info("logger.info-loginIndex");
+	logger.debug("logger.debug-loginIndex");
+	logger.trace("logger.trace-loginIndex");
 	return "loginIndex";
     }
 
